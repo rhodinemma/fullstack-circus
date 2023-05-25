23bfc7afeabdac88fb15f111e157d35d3b7d133f
rhodinemma <rhodinemma10@gmail.com>
2023-05-25 15:04:34 +0300

    feat: client-side rendering

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,4 +1,20 @@
+import BlogCard from "@/components/BlogCard/BlogCard";
+import { useEffect, useState } from "react";
+
 const Blog = () => {
+    const [blogData, setBlogData] = useState([]);
+
+    useEffect(() => {
+        async function fetchBlogData() {
+            const res = await fetch("/api/blog");
+            const data = await res.json();
+            setBlogData(data);
+            console.log(data)
+        }
+
+        fetchBlogData();
+    }, [])
+
     return <>
         <section className={blogSectionClasses.container}>
             <h2 className={blogSectionClasses.heading}>Blog</h2>
@@ -10,7 +26,9 @@ const Blog = () => {
         </section>
 
         <div className="grid gap-6 md:grid-cols-2">
-            { }
+            {blogData?.map((blog: any) => (
+                <BlogCard key={blog.title} author={blog.author} date={blog.date} description={blog.description} image={blog.image} title={blog.title} />
+            ))}
         </div>
     </>
 }
